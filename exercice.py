5f759d2c1bbc93f6a1c92f9064c835263bec5486
Jonathan-tsf <jonat.tapiero@gmail.com>
2020-09-09 05:06:53 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -9,7 +9,7 @@ def capitaliser_pays(nom):
     
     nom = ' '.join(nom)
     place = nom.find('-')
-    nom[place+1] = nom[place+1].upper()
+    list(nom)[place+1] = list(nom[place+1].upper()
     return nom
 
 
