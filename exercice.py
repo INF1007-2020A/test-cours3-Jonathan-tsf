f2cae737e53cbfb5f2826b359af48fe4282ef267
Jonathan-tsf <jonat.tapiero@gmail.com>
2020-09-09 05:03:08 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 def capitaliser_pays(nom):
     nom = nom.split(' ')
-    nom = [nom[i].split('-') for i in range(len(nom))]
     nom = [nom[i].lower() for i in range(len(nom))]
     for i in range(len(nom)):
         if nom[i] != "and":
